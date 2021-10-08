e83473f0e0f66233b0311bb3f871da11d43984e0
Mohamed-2000 <mogenaidy@gmail.com>
2021-10-08 21:40:51 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // update data base when app starting 
+            var host = CreateHostBuilder(args).Build();
+
+            // use using to specify the life time of the instance of StoreContext
+            using var scope = host.Services.CreateScope();
+            
+                //Get our Services
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                
+                // using try and catch because we don't have global Exception available here in Program class
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    //apply any pending migrations to the Database and create Db if it isn't exist
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch(Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An Error occured During migration");
+                }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
